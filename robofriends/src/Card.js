dc79c13aad3d3b694a2c99bb09f44b417ce5bcb0
iamfortune <ikechifortune@gmail.com>
2020-01-22 21:17:01 +0100

    destructured props to make code cleaner

--- robofriends/src/Card.js
+++ robofriends/src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = (props) => {
+const Card = ({ name, email, id }) => {
     return (
         <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img src={`https://robohash.org/${props.id}200x200`} alt='robots'/>
+            <img src={`https://robohash.org/${id}200x200`} alt='robots'/>
             <div>
-                <h2>{props.name}</h2>
-                <p>{props.email}</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     );
