389b7d1a43b819014d1247bcee1c239cef94bba4
iamfortune <ikechifortune@gmail.com>
2020-01-22 22:56:27 +0100

    added new Component called CardList

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Card from './Card';
+import CardList from './CardList';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import { robots } from './robots';
