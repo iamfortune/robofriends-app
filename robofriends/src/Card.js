1ccc3b1629206e4cf3843576f60ba69183f5c5fa
iamfortune <ikechifortune@gmail.com>
2020-01-22 21:12:15 +0100

    used props to make changes to robofriends

--- robofriends/src/Card.js
+++ robofriends/src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = (props) => {
     return (
         <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img src='https://robohash.org/divine?200x200' alt='robots'/>
+            <img src={`https://robohash.org/${props.id}200x200`} alt='robots'/>
             <div>
                 <h2>{props.name}</h2>
                 <p>{props.email}</p>
