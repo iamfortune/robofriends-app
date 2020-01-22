f4447c84cfee84fc4dbdbe3fe32a1c6ae5790ae9
iamfortune <ikechifortune@gmail.com>
2020-01-22 12:23:30 +0100

    added more components and styled with tachyons

--- robofriends/src/Card.js
+++ robofriends/src/Card.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Card = () => {
     return (
-        <div>
+        <div className='bg-light-green dib br3 pa3 ma2 grow'>
             <img src='https://robohash.org/divine?200x200' alt='robots'/>
             <div>
-                <h2>Kaycodev</h2>
-                <p>kay@gmail.com</p>
+                <h2>Divine Philip</h2>
+                <p>dpkreativ@gmail.com</p>
             </div>
         </div>
     );
--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -5,9 +5,11 @@ import Card from './Card';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
-ReactDOM.render(<Card />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(
+               <div>
+                    <Card />
+                    <Card />
+                    <Card />
+               </div>
+    , document.getElementById('root'));
 serviceWorker.unregister();
