0172918d36b80790c96e1807a72aac33ef7fe325
iamfortune <ikechifortune@gmail.com>
2020-01-22 15:36:48 +0100

    added props to card and linked to the db

--- robofriends/src/Card.js
+++ robofriends/src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = () => {
+const Card = (props) => {
     return (
         <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
             <img src='https://robohash.org/divine?200x200' alt='robots'/>
             <div>
-                <h2>Divine Philip</h2>
-                <p>dpkreativ@gmail.com</p>
+                <h2>{props.name}</h2>
+                <p>{props.email}</p>
             </div>
         </div>
     );
