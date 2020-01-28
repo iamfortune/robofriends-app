1609d20f0af2826964ee85d472f29bd6697e7203
iamfortune <ikechifortune@gmail.com>
2020-01-28 14:47:31 +0100

    modified files

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -2,6 +2,7 @@ import React, { Component }from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
 import SearchBox from './SearchBox';
+import './App.css';
 
 
 class App extends Component {
@@ -22,7 +23,7 @@ class App extends Component {
         })
         return (
             <div className='tc'>
-            <h1>Robofriends</h1>
+            <h1 className='f2'>Robofriends</h1>
             <SearchBox searchChange={this.onSearchChange}/>
             <CardList robots={ filteredRobots } />
             </div>
