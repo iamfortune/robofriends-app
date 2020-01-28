bd8e397679d84a9098fea67bf587f8fe270391fa
iamfortune <ikechifortune@gmail.com>
2020-01-28 11:55:11 +0100

    modified app, added state

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { Component }from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
 import SearchBox from './SearchBox';
 
-const App = () => {
-    return (
-        <div>
-        <h1>Robofriends</h1>
-        <SearchBox />
-        <CardList robots={robots} />
-        </div>
-    );
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: '',
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+    }
+    render() {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        })
+        return (
+            <div className='tc'>
+            <h1>Robofriends</h1>
+            <SearchBox searchChange={this.onSearchChange}/>
+            <CardList robots={ filteredRobots } />
+            </div>
+        );
+
+    }
 }
+   
 
 export default App;
