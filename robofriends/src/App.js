406a47e090716068c4d2874155e6d4d1049dc939
iamfortune <ikechifortune@gmail.com>
2020-02-16 17:20:04 +0100

    refactored code to add a loading page in case json was too slow in loading

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -26,14 +26,18 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
-        return (
-            <div className='tc'>
-            <h1 className='f2'>Robofriends</h1>
-            <SearchBox searchChange={this.onSearchChange}/>
-            <CardList robots={ filteredRobots } />
-            </div>
-        );
-
+        if (this.state.robots.length === 0) {
+            return <h1>Loading...</h1>
+        } else {
+            return (
+                <div className='tc'>
+                <h1 className='f2'>Robofriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={ filteredRobots } />
+                </div>
+            );
+    
+        }
     }
 }
    
