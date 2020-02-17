29a03a95d481dfe2f9ae97604c0075da9a5fdf23
iamfortune <ikechifortune@gmail.com>
2020-02-17 15:14:34 +0100

    added a style to the scroll component

--- robofriends/src/Scroll.js
+++ robofriends/src/Scroll.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Scroll = () => {
+const Scroll = (props) => {
     return (
-        <div style={{ overflowY: 'scroll', border: '1px solid black', height: '500px'}}>
+        <div style={{ overflowY: 'scroll', border: '5px solid black', height: '800px'}}>
             {props.children}
         </div>
     );
