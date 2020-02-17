5eb35560df1ec9a6f2e9e6224fa3994206b908d2
iamfortune <ikechifortune@gmail.com>
2020-02-17 20:57:22 +0100

    modified app component by refactoring code

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component }from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import Scroll from './Scroll.js';
